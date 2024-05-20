3c55a9b4153ba53c88a27fa8e349f5a187a957e3
priyankarpal <88102392+priyankarpal@users.noreply.github.com>
2024-05-20 21:17:38 +0530

    feat: routes

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import "dotenv/config";
 import express, { Application } from 'express';
-import router from "./routes/user.routes";
+import routes from "./routes/routes";
 
 const app: Application = express();
 
 // middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use("/", router);
+app.use(routes);
 
-app.listen(process.env.PORT || 3001, () => {
+app.listen(process.env.PORT || 3002, () => {
     console.log(`Server running on localhost:${process.env.PORT}`);
 });
