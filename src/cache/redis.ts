18462b05fab93a41ba43eb6b2efd01651d3094b2
priyankarpal <88102392+priyankarpal@users.noreply.github.com>
2024-05-23 23:46:20 +0530

    refactor

--- src/cache/redis.ts
+++ src/cache/redis.ts
@@ -6,14 +6,18 @@ interface RedisConfig {
     port: number;
     password?: string;
 }
-if (!process.env.REDIS_HOST || !process.env.REDIS_PORT || !process.env.REDIS_PASSWORD) {
-    throw new Error("Missing Redis configuration");
-}
+// if (!process.env.REDIS_HOST || !process.env.REDIS_PORT || !process.env.REDIS_PASSWORD) {
+//     throw new Error("Missing Redis configuration");
+// }
 const client = new Redis({
     host: process.env.REDIS_HOST!,
     port: parseInt(process.env.REDIS_PORT!),
     password: process.env.REDIS_PASSWORD!
 } as RedisConfig);
+
+client.on("connect", () => {
+    console.log("redis connected");
+});
 
 
 export default client;
